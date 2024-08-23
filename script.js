bb7bb4e69271759b7e984f7c8a0dfd299a2ab143
Lakshita <lakshitabinyala@gmail.com>
2024-08-23 16:04:30 +0530

    All done everything in working

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const hours=document.getElementsByClassName("hrs");
-const minutes=document.getElementsByClassName("mins");
-const seconds=document.getElementsByClassName("sec");
+const hours = document.getElementById('hrs')
+const minutes=document.getElementById("mins");
+const seconds=document.getElementById("sec");
+const ampm = document.getElementById("ampm");
 
 function updateClock(){
   let h=new Date().getHours();
    let m=new Date().getMinutes();
    let s= new Date().getSeconds();
 
-hours.innerText=h;
+// hours.innerText=h;
 minutes.innerText=m;
 seconds.innerText=s;
-setTimeout(()=>{
-
-},1000)
+if(h>=12){
+  ampm.innerText="PM";
+  hours.innerText=h-12;
+}
+else{
+  ampm.innerText="AM";
+}
+setTimeout(updateClock, 1000);
 }
 
 updateClock()
